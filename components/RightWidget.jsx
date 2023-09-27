8dccff841bb32c97e1ce15de1fcc8a49043f87a4
daniel-cox <ghostseal3@gmail.com>
2023-09-27 15:49:37 -0500

    fixed jsx pricing bug

--- components/RightWidget.jsx
+++ components/RightWidget.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
 
-
 const PurchaseItemButtons = () => {
   // Initialize the main price
   const mainPrice = 1575.00;
@@ -9,11 +8,11 @@ const PurchaseItemButtons = () => {
   // Create a state variable to store the selected protection plan price
   const [selectedPlanPrice, setSelectedPlanPrice] = useState(0);
 
-  const [selectedProtectionPlanPrice, setSelectedProtectionPlanPrice] = useState(0);
-
-
   // Create a state variable to track if the protection plan is selected
   const [protectionPlanSelected, setProtectionPlanSelected] = useState(false);
+
+  // Calculate the total price by adding the main price and selected plan price
+  const totalPrice = mainPrice + selectedPlanPrice;
 
   // Function to handle button click and update the selected protection plan price
   const handleButtonClick = (price) => {
@@ -28,8 +27,7 @@ const PurchaseItemButtons = () => {
       // Throw an error if no protection plan is selected
       throw new Error('Please select a protection plan before clicking "Buy Now".');
     } else {
-      // Calculate the total price by adding the main price and selected plan price
-      const totalPrice = mainPrice + selectedPlanPrice;
+      // You can now access the totalPrice variable here
       console.log(`Total Price: $${totalPrice.toFixed(2)}`);
     }
   };
@@ -37,7 +35,7 @@ const PurchaseItemButtons = () => {
   return (
     <div className='rightSidebar m-5'>
       {/* Display the total price */}
-      <h3 className='text-center'>Price: ${mainPrice.toFixed(2)}</h3>
+      <h3 className='text-center'>Price: ${totalPrice.toFixed(2)}</h3>
       <Button variant="warning mt-4 payBtn" onClick={handleBuyNowClick}>Buy Now</Button>
       <div className="row proBtn">
         <div className="h6 m-3">Protection Plan</div>
@@ -45,34 +43,37 @@ const PurchaseItemButtons = () => {
           {/* Use onClick to call the handleButtonClick function with the selected price */}
           <Button
             variant="light"
-            id='2yearPro'
+            id='1yearPro'
             onClick={() => handleButtonClick(350.00)}
           >
-            2 year - $350.00
+            1 year - $350.00
           </Button>
         </div>
         <div className="col-3 proBtn">
+          {/* Use onClick to call the handleButtonClick function with the selected price */}
+          <Button
+            variant="light"
+            id='2yearPro'
+            onClick={() => handleButtonClick(650.00)}
+          >
+            2 year - $650.00
+          </Button>
+        </div>
+        <div className="col-3 proBtn">
+          {/* Use onClick to call the handleButtonClick function with the selected price */}
           <Button
             variant="light"
             id='3yearPro'
-            onClick={() => handleButtonClick(425.00)}
+            onClick={() => handleButtonClick(1050.00)}
           >
-            3 year - $425.00
+            2 year - $1050.00
           </Button>
         </div>
         <div className="col-3 proBtn">
+          {/* Use onClick to call the handleButtonClick function with the selected price */}
           <Button
             variant="light"
-            id='4yearPro'
-            onClick={() => handleButtonClick(500.00)}
-          >
-            4 year - $500.00
-          </Button>
-        </div>
-        <div className="col-3 proBtn">
-          <Button
-            variant="light"
-            id='4yearPro'
+            id='noPro'
             onClick={() => handleButtonClick(0.00)}
           >
             No Protection
@@ -82,4 +83,5 @@ const PurchaseItemButtons = () => {
     </div>
   );
 };
+
 export default PurchaseItemButtons;
--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import './App.css'
 function App() {
   return (
     <div>
-      <Navigation />
+      {/* <Navigation /> */}
       <MainContent />
     </div>
 )}

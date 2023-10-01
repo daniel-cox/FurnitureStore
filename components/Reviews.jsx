d93ac49e8622d7937f16dab93ac6213d16b53a46
ShreddShark <daniel.cox2685@gmail.com>
2023-10-01 14:49:26 -0500

    new jsx

--- components/Reviews.jsx
+++ components/Reviews.jsx
@@ -9,8 +9,12 @@ function Reviews() {
                     <div className='outterBox'>
                         <div className='Ratings'>
                             <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
                                 <div className='comment'>
-
+                                    <p className='review'>I couldn't be happier with my new leather couch! It's not only stylish but incredibly comfortable. The quality of the leather is outstanding, and it adds a touch of elegance to my living room. Thank you for this fantastic addition to my home!</p>
                                 </div>
                         </div>
                     </div> 
---
+++ components/global/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="container-fluid">
+        <div className="row">
+            <div className="col">
+                 
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer

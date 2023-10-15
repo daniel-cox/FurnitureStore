e2befb67b1ce97790bfd43b02863144b47fdbce2
ShreddShark <daniel.cox2685@gmail.com>
2023-10-15 09:21:38 -0500

    homepage edits

--- components/Homepage.jsx
+++ components/Homepage.jsx
@@ -4,8 +4,12 @@ import React from 'react'
 function Homepage() {
   return (
     <div>
-      <h1>test</h1>
-      <img src="./images/hero.jpg" alt="" className='heroImg' />
+      <div className="container-fluid p-0">
+        <img src="./public/hero.jpg" alt="hero image" className='heroIm w-100' />
+      </div>
+      <div className="productGrid">
+        <h2 className='text-center'>Newest Items In Our Collection</h2>
+      </div>
     </div>
   )
 }
